d9c5dd6bb7dd5f77b330f4e9c52879b592f0a8f6
lassejlv <vestergaardlasse2@gmail.com>
2026-01-15 01:20:34 +0100

    Ivar is awesome!

--- web/src/routes/api.auth.$.ts
+++ web/src/routes/api.auth.$.ts
@@ -1,8 +1,16 @@
 import { auth } from '@/lib/auth'
 import { createFileRoute } from '@tanstack/react-router'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
 
 const app = new Hono().basePath('/api/auth')
+
+app.use(
+  cors({
+    origin: ['http://localhost:1420'],
+    credentials: true,
+  }),
+)
 
 app.on(['POST', 'GET'], '/*', (c) => {
   return auth.handler(c.req.raw)
